be64d9a744e2b9f73c253f740c125ef081b24d40
DormantOne <131829942+DormantOne@users.noreply.github.com>
2023-11-21 05:40:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
-document.getElementById('trueButton').addEventListener('click', () => generateQR('True'));
-document.getElementById('falseButton').addEventListener('click', () => generateQR('False'));
-document.getElementById('submitQR').addEventListener('click', sendEmail);
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('trueButton').addEventListener('click', () => generateQR('True'));
+    document.getElementById('falseButton').addEventListener('click', () => generateQR('False'));
+    document.getElementById('submitQR').addEventListener('click', sendEmail);
 
-var qrValue = '';
-var docId = '';
+    var qrValue = '';
+    var docId = '';
 
-function generateQR(answer) {
-    var username = document.getElementById('username').value;
-    qrValue = `Username: ${username}, Answer: ${answer}`;
+    function generateQR(answer) {
+        var username = document.getElementById('username').value;
+        qrValue = `Username: ${username}, Answer: ${answer}`;
 
-    var qr = new QRious({
-        element: document.getElementById('qrCodeCanvas'),
-        size: 200,
-        value: qrValue
-    });
+        var qr = new QRious({
+            element: document.getElementById('qrCodeCanvas'),
+            size: 200,
+            value: qrValue
+        });
 
-    var qrCodeCanvas = document.getElementById('qrCodeCanvas');
-    var qrCodeURL = qrCodeCanvas.toDataURL("image/png");
+        document.getElementById('qrCodeCanvasContainer').style.display = 'block';
 
-    firebase.firestore().collection('qrcodes').add({
-        username: username,
-        answer: answer,
-        url: qrCodeURL,
-        timestamp: new Date()
-    }).then(docRef => {
-        docId = docRef.id;
-    });
-}
+        var qrCodeCanvas = document.getElementById('qrCodeCanvas');
+        var qrCodeURL = qrCodeCanvas.toDataURL("image/png");
 
-function sendEmail() {
-    if (!qrValue || !docId) {
-        alert('Please generate a QR code first.');
-        return;
+        firebase.firestore().collection('qrcodes').add({
+            username: username,
+            answer: answer,
+            url: qrCodeURL,
+            timestamp: new Date()
+        }).then(docRef => {
+            docId = docRef.id;
+        });
     }
 
-    var username = document.getElementById('username').value;
-    var email = username + '@upmc.edu';
-    var subject = encodeURIComponent('Your QR Code for Food for Thought');
-    var body = encodeURIComponent('Access your QR code here: ') + 
-               encodeURIComponent(`https://dormantone.github.io/foodforthought/qrDisplay.html?docId=${docId}`);
+    function sendEmail() {
+        if (!qrValue || !docId) {
+            alert('Please generate a QR code first.');
+            return;
+        }
 
-    var mailtoLink = 'mailto:' + email + '?subject=' + subject + '&body=' + body;
-    window.open(mailtoLink, '_blank');
-}
+        var username = document.getElementById('username').value;
+        var email = username + '@upmc.edu';
+        var subject = encodeURIComponent('Your QR Code for Food for Thought');
+        var body = encodeURIComponent('Access your QR code here: ') + 
+                   encodeURIComponent(`https://dormantone.github.io/foodforthought/qrDisplay.html?docId=${docId}`);
+
+        var mailtoLink = 'mailto:' + email + '?subject=' + subject + '&body=' + body;
+        window.open(mailtoLink, '_blank');
+    }
+});
