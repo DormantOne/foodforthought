0e6f828125800b5780c025fe2f221415aea37f3b
DormantOne <131829942+DormantOne@users.noreply.github.com>
2023-11-20 21:06:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 document.getElementById('trueButton').addEventListener('click', generateQR);
 document.getElementById('falseButton').addEventListener('click', generateQR);
-// The line below is not needed as the email is sent from within the generateQR function
-// document.getElementById('emailButton').addEventListener('click', sendEmail);
 
 function generateQR() {
     var qr = new QRious({
@@ -23,14 +21,12 @@ function generateQR() {
 }
 
 function sendEmail(docId) {
-    var username = document.getElementById('username').value;
-    var email = username + '@upmc.edu';
+    var emailPrefix = document.getElementById('emailPrefix').value;
+    var email = emailPrefix + '@upmc.edu'; // Concatenate with company email domain
     var subject = encodeURIComponent('Your QR Code for Food for Thought');
     
-    // Include the repository name in the URL
     var body = encodeURIComponent('Access your QR code here: ') + encodeURIComponent(`https://dormantone.github.io/foodforthought/qrDisplay.html?docId=${docId}`);
 
     var mailtoLink = 'mailto:' + email + '?subject=' + subject + '&body=' + body;
     window.open(mailtoLink, '_blank');
 }
-
