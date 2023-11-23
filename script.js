0db1bfb59c5f52e563d4ee70c2b405d6d7de3d8b
DormantOne <131829942+DormantOne@users.noreply.github.com>
2023-11-23 10:54:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,44 +3,47 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('falseButton').addEventListener('click', () => generateQR('False'));
     document.getElementById('submitQR').addEventListener('click', sendEmail);
 
-    var qrValue = '';
     var docId = '';
 
-  function generateQR(answer) {
-    var username = document.getElementById('username').value;
-    var qrValue = `Username: ${username}, Answer: ${answer}`;
+    function generateQR(answer) {
+        var username = document.getElementById('username').value;
+        var qrValue = `Username: ${username}, Answer: ${answer}`;
 
-    var qr = new QRious({
-        element: document.getElementById('qrCodeCanvas'),
-        size: 200,
-        value: qrValue // The actual data you want to encode in the QR code
-    });
+        var qr = new QRious({
+            element: document.getElementById('qrCodeCanvas'),
+            size: 200,
+            value: qrValue // The actual data you want to encode in the QR code
+        });
 
-    document.getElementById('qrCodeCanvasContainer').style.display = 'block';
+        document.getElementById('qrCodeCanvasContainer').style.display = 'block';
 
-    var qrCodeURL = document.getElementById('qrCodeCanvas').toDataURL("image/png");
+        var qrCodeDataURL = document.getElementById('qrCodeCanvas').toDataURL("image/png");
 
-    // Store the QR code URL in Firestore
-    firebase.firestore().collection('qrcodes').add({
-        url: qrCodeURL,
-        timestamp: new Date()
-    }).then(docRef => {
-        docId = docRef.id; // Save the document ID for use in the email
-    });
-}
-
-function sendEmail() {
-    if (!docId) {
-        alert('Please generate a QR code first.');
-        return;
+        // Store the QR code data URL in Firestore
+        firebase.firestore().collection('qrcodes').add({
+            qrCodeDataUrl: qrCodeDataURL,
+            username: username,
+            question: 'Is the human brain composed of over 60% fat?',
+            answer: answer,
+            timestamp: new Date()
+        }).then(docRef => {
+            docId = docRef.id; // Save the document ID for use in the email
+        });
     }
 
-    var username = document.getElementById('username').value;
-    var email = username + '@upmc.edu';
-    var subject = encodeURIComponent('Your QR Code for Food for Thought');
-    var body = encodeURIComponent('Access your QR code data here: ') + 
-               encodeURIComponent(`https://dormantone.github.io/foodforthought/qrDisplay.html?docId=${docId}`);
+    function sendEmail() {
+        if (!docId) {
+            alert('Please generate a QR code first.');
+            return;
+        }
 
-    var mailtoLink = 'mailto:' + email + '?subject=' + subject + '&body=' + body;
-    window.open(mailtoLink, '_blank');
-}
+        var username = document.getElementById('username').value;
+        var email = username + '@upmc.edu';
+        var subject = encodeURIComponent('Your QR Code for Food for Thought');
+        var body = encodeURIComponent('Access your QR code data here: ') + 
+                   encodeURIComponent(`https://dormantone.github.io/foodforthought/qrDisplay.html?docId=${docId}`);
+
+        var mailtoLink = 'mailto:' + email + '?subject=' + subject + '&body=' + body;
+        window.open(mailtoLink, '_blank');
+    }
+});
