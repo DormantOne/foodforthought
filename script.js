c6ecb817459ebd195a39b0e0fae3ab7ec65b9ff7
DormantOne <131829942+DormantOne@users.noreply.github.com>
2023-11-24 17:25:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('trueButton').addEventListener('click', () => generateQR('True'));
-    document.getElementById('falseButton').addEventListener('click', () => generateQR('False'));
-    document.getElementById('submitQR').addEventListener('click', sendEmail);
+    var trueButton = document.getElementById('trueButton');
+    var falseButton = document.getElementById('falseButton');
+    var submitButton = document.getElementById('submitQR');
+
+    trueButton.addEventListener('click', () => generateQR('True'));
+    falseButton.addEventListener('click', () => generateQR('False'));
+    submitButton.addEventListener('click', sendEmail);
 
     var docId = '';
 
     function generateQR(answer) {
         var username = document.getElementById('username').value;
-        var question = 'Is the human brain composed of over 60% fat?'; // Your actual question
+        var question = 'Is the human brain composed of over 60% fat?'; 
         var encodedQuestion = encodeURIComponent(question);
         var encodedAnswer = encodeURIComponent(answer);
 
-        // Construct the URL for the QR code
         var formPageUrl = `https://dormantone.github.io/foodforthought/formPage.html?username=${encodeURIComponent(username)}&question=${encodedQuestion}&answer=${encodedAnswer}`;
 
         var qr = new QRious({
             element: document.getElementById('qrCodeCanvas'),
             size: 200,
-            value: formPageUrl // Use the form page URL as the QR code value
+            value: formPageUrl
         });
 
-        document.getElementById('qrCodeCanvasContainer').style.display = 'block';
+        document.getElementById('qrCodeCanvasContainer').style.display = 'none';
 
-        // Store the QR code data URL in Firestore
         var qrCodeDataURL = document.getElementById('qrCodeCanvas').toDataURL("image/png");
         firebase.firestore().collection('qrcodes').add({
             qrCodeDataUrl: qrCodeDataURL,
@@ -31,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
             answer: answer,
             timestamp: new Date()
         }).then(docRef => {
-            docId = docRef.id; // Save the document ID for use in the email
+            docId = docRef.id; 
         });
     }
 
@@ -41,19 +43,21 @@ document.addEventListener('DOMContentLoaded', function() {
             return;
         }
 
+        trueButton.disabled = true;
+        falseButton.disabled = true;
+        submitButton.disabled = true;
+
         var username = document.getElementById('username').value;
         var email = username + '@upmc.edu';
         var subject = 'Your QR Code for Food for Thought';
         var body = `Access your QR code data here: https://dormantone.github.io/foodforthought/qrDisplay.html?docId=${docId}`;
 
-        // Prepare payload for the HTTP request
         var payload = {
             email: email,
             subject: subject,
             body: body
         };
 
-        // Call the Firebase Cloud Function to send the email
         fetch('https://us-central1-foodforthought-e29a4.cloudfunctions.net/sendEmail', {
             method: 'POST',
             headers: {
@@ -63,10 +67,17 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         .then(response => response.json())
         .then(data => {
+            document.getElementById('emailStatus').innerText = 'Email sent successfully.';
             console.log('Email sent successfully:', data);
         })
         .catch((error) => {
+            document.getElementById('emailStatus').innerText = 'Error sending email. Please try again.';
             console.error('Error sending email:', error);
+        })
+        .finally(() => {
+            trueButton.disabled = false;
+            falseButton.disabled = false;
+            submitButton.disabled = false;
         });
     }
 });
