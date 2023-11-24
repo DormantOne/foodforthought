2998ceea6e433a0df40f56a2ef07f72875344f5e
DormantOne <131829942+DormantOne@users.noreply.github.com>
2023-11-24 18:19:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var trueAnswer = document.getElementById('trueAnswer');
     var falseAnswer = document.getElementById('falseAnswer');
 
+    // Initially, the QR code submit button is disabled
+    submitQRButton.disabled = true;
+
     submitAnswerButton.addEventListener('click', submitAnswer);
     submitQRButton.addEventListener('click', sendEmail);
 
@@ -11,79 +14,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function submitAnswer() {
         var selectedAnswer = trueAnswer.checked ? 'True' : falseAnswer.checked ? 'False' : null;
+        var correctAnswer = 'True'; // Assuming 'True' is the correct answer
+
         if (!selectedAnswer) {
             alert('Please select an answer.');
             return;
         }
-        generateQR(selectedAnswer);
+
+        if (selectedAnswer === correctAnswer) {
+            document.getElementById('emailStatus').innerText = 'Correct! You can now send your QR coupon.';
+            generateQR(selectedAnswer);
+            submitQRButton.disabled = false;
+        } else {
+            document.getElementById('emailStatus').innerText = 'Wrong answer. Please try again.';
+        }
     }
 
     function generateQR(answer) {
         var username = document.getElementById('username').value;
-        var question = 'Is the human brain composed of over 60% fat?'; 
-        var encodedQuestion = encodeURIComponent(question);
-        var encodedAnswer = encodeURIComponent(answer);
-
-        var formPageUrl = `https://yourwebsite.com/formPage.html?username=${encodeURIComponent(username)}&question=${encodedQuestion}&answer=${encodedAnswer}`;
-
-        var qr = new QRious({
-            element: document.getElementById('qrCodeCanvas'),
-            size: 200,
-            value: formPageUrl
-        });
-
-        document.getElementById('qrCodeCanvasContainer').style.display = 'none';
-
-        var qrCodeDataURL = document.getElementById('qrCodeCanvas').toDataURL("image/png");
-        firebase.firestore().collection('qrcodes').add({
-            qrCodeDataUrl: qrCodeDataURL,
-            username: username,
-            question: question,
-            answer: answer,
-            timestamp: new Date()
-        }).then(docRef => {
-            docId = docRef.id; 
-        });
+        // [Rest of the generateQR function]
     }
 
-    function sendEmail() {
-        if (!docId) {
-            alert('Please generate a QR code first.');
-            return;
-        }
-
-        var username = document.getElementById('username').value;
-        var email = username + '@upmc.edu';
-        var subject = 'Your QR Code for Food for Thought';
-        var body = `Access your QR code data here: https://yourwebsite.com/qrDisplay.html?docId=${docId}`;
-
-        var payload = {
-            email: email,
-            subject: subject,
-            body: body
-        };
-
-        fetch('https://us-central1-foodforthought-e29a4.cloudfunctions.net/sendEmail', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(payload)
-        })
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('emailStatus').innerText = 'Email sent successfully. Button will be reactivated in 3 minutes.';
-            console.log('Email sent successfully:', data);
-
-            submitQRButton.disabled = true;
-            setTimeout(() => {
-                submitQRButton.disabled = false;
-                document.getElementById('emailStatus').innerText = '';
-            }, 180000); // 180000 milliseconds = 3 minutes
-        })
-        .catch((error) => {
-            document.getElementById('emailStatus').innerText = 'Error sending email. Please try again.';
-            console.error('Error sending email:', error);
-        });
-    }
+    // [Rest of the sendEmail function]
 });
