6be422aabcc071ea703bc4d13555ff31dff17dce
DormantOne <131829942+DormantOne@users.noreply.github.com>
2023-11-24 18:52:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var trueAnswer = document.getElementById('trueAnswer');
     var falseAnswer = document.getElementById('falseAnswer');
 
-    submitQRButton.disabled = true; // Initially disable the QR code submit button
-    console.log("Submit QR Button initially disabled");
+    // Initially disable the QR code submit button
+    submitQRButton.disabled = true;
 
     submitAnswerButton.addEventListener('click', submitAnswer);
     submitQRButton.addEventListener('click', sendEmail);
@@ -14,25 +14,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function submitAnswer() {
         var selectedAnswer = trueAnswer.checked ? 'True' : falseAnswer.checked ? 'False' : null;
-        console.log("Selected answer:", selectedAnswer);
-
         var correctAnswer = 'True'; // Assuming 'True' is the correct answer
 
         if (!selectedAnswer) {
-            console.log("No answer selected");
             alert('Please select an answer.');
             return;
         }
 
         if (selectedAnswer === correctAnswer) {
-            console.log("Correct answer");
-            document.getElementById('emailStatus').innerText = 'Correct! You can now send your QR coupon.';
             generateQR(selectedAnswer);
             submitQRButton.disabled = false;
-            console.log("Submit QR Button should now be enabled");
         } else {
-            console.log("Incorrect answer");
-            document.getElementById('emailStatus').innerText = 'Wrong answer. Please try again.';
+            alert('Wrong answer. Please try again.');
             submitQRButton.disabled = true;
         }
     }
@@ -43,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function() {
         var encodedQuestion = encodeURIComponent(question);
         var encodedAnswer = encodeURIComponent(answer);
 
-        var formPageUrl = `https://yourwebsite.com/formPage.html?username=${encodeURIComponent(username)}&question=${encodedQuestion}&answer=${encodedAnswer}`;
+        var formPageUrl = `https://dormantone.github.io/foodforthought/formPage.html?username=${encodeURIComponent(username)}&question=${encodedQuestion}&answer=${encodedAnswer}`;
 
         var qr = new QRious({
             element: document.getElementById('qrCodeCanvas'),
@@ -61,15 +54,12 @@ document.addEventListener('DOMContentLoaded', function() {
             answer: answer,
             timestamp: new Date()
         }).then(docRef => {
-            docId = docRef.id; 
-            console.log("QR generated and doc ID set:", docId);
+            docId = docRef.id;
         });
     }
 
     function sendEmail() {
-        console.log("Attempting to send email");
         if (!docId) {
-            console.log("No QR code generated, cannot send email");
             alert('Please generate a QR code first.');
             return;
         }
@@ -77,7 +67,7 @@ document.addEventListener('DOMContentLoaded', function() {
         var username = document.getElementById('username').value;
         var email = username + '@upmc.edu';
         var subject = 'Your QR Code for Food for Thought';
-        var body = `Access your QR code data here: https://yourwebsite.com/qrDisplay.html?docId=${docId}`;
+        var body = `Access your QR code data here: https://dormantone.github.io/foodforthought/qrDisplay.html?docId=${docId}`;
 
         var payload = {
             email: email,
@@ -94,17 +84,9 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         .then(response => response.json())
         .then(data => {
-            document.getElementById('emailStatus').innerText = 'Email sent successfully. Button will be reactivated in 3 minutes.';
             console.log('Email sent successfully:', data);
-
-            submitQRButton.disabled = true;
-            setTimeout(() => {
-                submitQRButton.disabled = false;
-                document.getElementById('emailStatus').innerText = '';
-            }, 180000); // 180000 milliseconds = 3 minutes
         })
         .catch((error) => {
-            document.getElementById('emailStatus').innerText = 'Error sending email. Please try again.';
             console.error('Error sending email:', error);
         });
     }
